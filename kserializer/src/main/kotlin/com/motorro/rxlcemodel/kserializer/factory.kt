9fbb131aa8653aee5504fbb2f68cb37efb1885fd
motorro <motorro@gmail.com>
2020-01-08 17:47:55 +0300

    Extension to configure cache delegate with kotlin serializer. References: #14

---
+++ kserializer/src/main/kotlin/com/motorro/rxlcemodel/kserializer/factory.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Nikolai Kotchetkov.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.motorro.rxlcemodel.kserializer
+
+import com.motorro.rxlcemodel.base.entity.EntityValidatorFactory
+import com.motorro.rxlcemodel.base.service.SyncDelegateCacheService
+import com.motorro.rxlcemodel.base.service.stringifyParams
+import com.motorro.rxlcemodel.disklrucache.DiskLruCacheSyncDelegate
+import com.motorro.rxlcemodel.disklrucache.DiskLruCacheSyncDelegate.DiskLruCacheProvider
+import com.motorro.rxlcemodel.disklrucache.createDefaultDelegatePrefix
+import com.motorro.rxlcemodel.disklrucache.createDelegate
+import kotlinx.serialization.KSerializer
+
+/**
+ * Creates DiskLRU caching delegate for [SyncDelegateCacheService] that accepts Kotlin-serializable data
+ *
+ * @receiver Cache provider
+ * @param validatorFactory Entity validation factory (defines cache TTL)
+ * @param serializer Data serializer/deserializer
+ * @param prefix Caching name prefix to distinguish cache files from other delegates within the same cache directory
+ * @param stringify As [DiskLruCacheSyncDelegate] uses string params to create cache keys we should substitute
+ * data identifying parameters with string using [stringifyParams]
+ */
+inline fun <reified D: Any, P: Any> DiskLruCacheProvider.withKotlin(
+    validatorFactory: EntityValidatorFactory,
+    serializer: KSerializer<D>,
+    prefix: String = createDefaultDelegatePrefix(D::class.java),
+    crossinline stringify: P.() -> String = { toString() }
+) : SyncDelegateCacheService.Delegate<D, P> = createDelegate(
+    prefix = prefix,
+    sd = KotlinCacheDelegateSerializer(
+        validatorFactory = validatorFactory,
+        kSerializer = serializer
+    ),
+    stringify = stringify
+)
