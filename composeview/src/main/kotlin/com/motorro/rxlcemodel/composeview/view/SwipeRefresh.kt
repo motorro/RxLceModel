24463a22314de81c225166991188a22ee4a54719
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-09-13 14:14:56 +0200

    Kotlin 1.9.10

--- composeview/src/main/kotlin/com/motorro/rxlcemodel/composeview/view/SwipeRefresh.kt
+++ composeview/src/main/kotlin/com/motorro/rxlcemodel/composeview/view/SwipeRefresh.kt
@@ -1,36 +1,45 @@
-/*
- * Copyright 2021 Nikolai Kotchetkov.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *    http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.motorro.rxlcemodel.composeview.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 /**
  * Swipe-refresh content wrapper
  */
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SwipeRefresh(
     refreshing: Boolean,
     onRefresh: () -> Unit,
-    content: @Composable () -> Unit
+    content: @Composable (Boolean) -> Unit
 ) {
-    SwipeRefresh(
-        state = rememberSwipeRefreshState(isRefreshing = refreshing),
-        onRefresh = {
-            onRefresh()
-        },
-        content = { content() }
+    var refreshingStatus by remember("swipe_refresh_status") {
+        mutableStateOf(refreshing)
+    }
+    val refreshState = rememberPullRefreshState(refreshing = refreshing, onRefresh = {
+        refreshingStatus = false
+        onRefresh()
+    })
+    Box(
+        modifier = Modifier.pullRefresh(refreshState),
+        content = {
+            content(refreshingStatus)
+            PullRefreshIndicator(
+                refreshing = refreshing,
+                state = refreshState,
+                modifier = Modifier.align(Alignment.TopCenter),
+                scale = true
+            )
+        }
     )
 }
